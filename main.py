7c585d93da0faa7b738bf0074d196e0ea72191d3
jtaenzer <mars.taenzer@gmail.com>
2019-03-13 15:52:57 +0200

    simplified, moved some code elsewhere

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
-import random
+import operator
 import schedule as sched
+import games as games
 
-def coin_flip():
-  return random.randint(0,1)
 
-def weighted_coin_flip(weight1,weight2):
-  return random.randint(0,weight1+weight2) > weight1
+def print_standings_sorted(standings):
+  standings_sorted = sorted(standings.items(), key=lambda kv: kv[1]['points'], reverse=True)
+  print '{:<25}'.format('Team'), '{:<10}'.format('Wins'), '{:<10}'.format('Losses'), '{:<10}'.format('OT Losses'), '{:<10}'.format('Points')
+  for team in xrange(len(standings_sorted)): 
+    print '{:<25}'.format(standings_sorted[team][0]), '{:<10}'.format(standings_sorted[team][1]["wins"]), '{:<10}'.format(standings_sorted[team][1]["losses"]), '{:<10}'.format(standings_sorted[team][1]["OTlosses"]), '{:<10}'.format(standings_sorted[team][1]["points"])
 
-def generate_initial_standings(teams):
-  standings = {}
-  for team in teams:
-    standings[team] = {"wins":0, "losses":0}
-  return standings
+teams_file = open("teams.txt","r")
+teams = []
+for line in teams_file: teams.append(line.replace("\n",""))
 
-def play_games(teams, schedule):
-  standings = generate_initial_standings(teams)
-  for day in schedule:
-    for game in schedule[day]:
-      winner=coin_flip()
-      if winner==0: 
-        standings[game[0]]["wins"]+=1
-        standings[game[1]]["losses"]+=1
-      elif winner==1:
-        standings[game[1]]["wins"]+=1
-        standings[game[0]]["losses"]+=1
-  return standings
-
-teams = ["Calgary","Edmonton", "San Jose", "Colorado", "Pheonix"]
 schedule = sched.generate_schedule_simple(teams, 82)
-standings = play_games(teams, schedule)
+standings = games.play_games_simple(teams, schedule)
+print_standings_sorted(standings)
 
-print "team", "wins", "losses"
-for team in standings: 
-  print team, standings[team]["wins"], standings[team]["losses"]
+"""
+for i in range(1000):
+  print "running iteration :", i
+  standings = games.play_games_simple(teams, schedule)
+"""
+
+
