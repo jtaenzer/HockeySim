9fac422d1e2a1b095288196ab65a7b2d846852ca
jtaenzer <mars.taenzer@gmail.com>
2019-03-18 17:21:01 +0200

    updated generate_schedule_simple for the new expected format of the schedule dictionary

--- schedule.py
+++ schedule.py
@@ -1,64 +1,57 @@
 import random, copy
 
-# Generates random games to put in the schedule
-def generate_games_simple(teams):
-  games = []
-  # Deepcopy teams so we can remove entries as we generate games without affecting the original list
-  teams_remaining = copy.deepcopy(teams)
-
-  # Loop until there is at most 1 team remaining, in case the number of teams is uneven
-  while len(teams_remaining)>1:
-    home=random.choice(teams_remaining) 
-    teams_remaining.remove(home)
-    away=random.choice(teams_remaining)
-    teams_remaining.remove(away)
-    games.append([home,away])
-  return teams_remaining, games
-
-# Generates an extra game, used if there is an uneven number of teams
-def generate_remainder_game_simple(remainder,teams):
-  games = []
-  home = remainder[0]
-  teams.remove(home)
-  away=random.choice(teams)
-  games.append([home,away])
-  return away,games
-
 # Generate a random schedule where every team in teams will play N games
 # Missing rules:
 # 1. Balancing home/away games
 # 2a. Ensuring each team plays each other team the same number of times
-# 2b. Ensuring each team plays each other team the correct number of times
+# 2b. Ensuring each team plays each other team the correct number of times (based on e.g. divisional/conference rules)
 # ...Probably more
 # This should only be used for debugging in its current state
 def generate_schedule_simple(teams, Ngames):
   schedule = {}
   extra = "" # extra is used to keep track of teams that have an "extra" game due to an uneven number of teams
-  daycounter=1
+  gamecounter=0
+  daycounter=0
 
   # Loop until the generated schedule has Ngames games for every team
   while chk_schedule_simple(teams, schedule, Ngames)==False:
-    key="day"+str(daycounter) # key for the schedule dictionary
+    key="game"+str(daycounter) # key for the schedule dictionary
 
-    # Deepcopy teams so we can remove entries when a remainder game is necessary
-    teams_mod = copy.deepcopy(teams)
+    # Deepcopy teams so we can remove entries without affecting the original list
+    teams_local = copy.deepcopy(teams)
 
     # If extra is non-empty, we generated a remainder game in the previous iteration
-    # so we remove the team that played an extra game from teams_mod
+    # so we remove the team that played an extra game from teams_local
     if extra:
-      teams_mod.remove(extra)
+      teams_local.remove(extra)
       extra = ""
 
-    # Generate a set of games
-    remainder, schedule[key] = generate_games_simple(teams_mod)
+    # Add games to schedule by choosing teams at random from teams_remaining
+    # Teams chosen for games are removed from teams_remaining until at most 1 is left, which will happen if len(teams) is odd
+    teams_remaining=copy.deepcopy(teams_local)
+    while len(teams_remaining)>1:
+      home=random.choice(teams_remaining);
+      teams_remaining.remove(home)
+      away=random.choice(teams_remaining);
+      teams_remaining.remove(away)
+      schedule["game"+str(gamecounter)]={ "date":"day"+str(daycounter), "visitor":away, "home":home }
+      gamecounter+=1
 
-    # If remainder is non-empty, we have an uneven number of teams
-    # So we generate an extra game game for the team that was left out on this iteration
-    if len(remainder)>0:
+    # If len(teams) is odd, we will have a "remainder" team that didn't get a game scheduled in the above while loop
+    # Schedule a game for that team with  another random team
+    # The team with an "extra" game will be removed from teams_local / teams_remaining in the next loop iteration
+    if len(teams_remaining)>0:
       daycounter+=1
-      key="day"+str(daycounter)
-      extra,schedule[key]=generate_remainder_game_simple(remainder,teams_mod)
-    daycounter+=1
+      remainder=teams_remaining[0]
+      teams_local.remove(remainder)
+      extra=random.choice(teams_local)
+      teams_remaining.append(extra)
+      home=random.choice(teams_remaining);
+      teams_remaining.remove(home)
+      away=random.choice(teams_remaining);
+      teams_remaining.remove(away)
+      schedule["game"+str(gamecounter)]={ "date":"day"+str(daycounter), "visitor":away, "home":home }
+      gamecounter+=1
 
   return schedule
 
