6b6461c1b146038ee3ed3b51602763c4793b0842
jtaenzer <mars.taenzer@gmail.com>
2019-03-24 17:02:20 +0200

    removed commented code

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from __future__ import division
 
-import random
-
 from simulation import simulation
-from schedule_maker import schedule_maker
-from play_season import play_season
 from visualize import visualize
 
 ##############################
@@ -32,34 +28,3 @@ for i in xrange(sims):
 
 plotter.write_TFile()
 #plotter.draw_TGraphs()
-
-"""
-
-schedmaker = schedule_maker("NHL", teams, Ngames, schedule_file)
-if not schedmaker.schedule: print "Schedule is empty"
-
-
-else:
-
-
-
-# Run iterations of the same season
-  for sim in xrange(sims):
-    print "Running sim #", sim
-    print ''
-    # Make the result dictionary based on the teams
-    result=prep_sim_result(teams)
-
-    for i in xrange(iterations):
-      season_18_19 = play_season(file_path, schedmaker.schedule)
-      season_18_19.play_games_simple()
-      season_18_19.determine_playoffs_NHL(result)
-
-    # Only print the sim results if we ran multiple iterations
-    if iterations>1: 
-      print_sim_result(result, "Playoff %")
-      plotter.fill_TTree(sim, result)
-
-plotter.write_TFile()
-#plotter.draw_TGraphs()
-"""
