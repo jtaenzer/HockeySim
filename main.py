2a9e66b25271f866c84cb64060e21a7ec890354c
jtaenzer <mars.taenzer@gmail.com>
2019-03-25 12:47:19 +0200

    cleanup and modifications for team_info class

--- main.py
+++ main.py
@@ -9,22 +9,27 @@ from visualize import visualize
 teams_file = "/home/joe/Desktop/fun/HockeySim/teams/NHL_2018-2019.txt"
 schedule_file="/home/joe/Desktop/fun/HockeySim/schedules/NHL_2018-2019.csv"
 
-sims = 2
+sims       = 2
 iterations = 1000
-doPlotting=True
-rootfile="test.root"
-treename="tree"
+doPlotting = True
+rootfile   = "test.root"
+treename   = "tree"
 
 sim = simulation(iterations, "NHL", teams_file, schedule_file)
+plotter = None
+
+team_names = []
+for team in sim.teams:
+    team_names.append(team.name)
 
 if doPlotting:
-  plotter = visualize(rootfile,treename,sim.teams)
-  plotter.set_weight(100/iterations)
+    plotter = visualize(rootfile, treename, team_names)
+    plotter.set_weight(100/iterations)
 
 for i in xrange(sims):
-  sim.run_simulation()
-  if doPlotting:
-    plotter.fill_TTree(i, sim.result)
+    sim.run_simulation()
+    if doPlotting:
+        plotter.fill_TTree(i, sim.result)
 
 plotter.write_TFile()
 #plotter.draw_TGraphs()
