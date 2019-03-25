a347dc161b7b854db41f68c22f24462068c24ccc
jtaenzer <mars.taenzer@gmail.com>
2019-03-25 12:47:24 +0200

    cleanup and modifications for team_info class

--- play_season.py
+++ play_season.py
@@ -1,9 +1,10 @@
 import random, copy
+from team_info import team_info
 
 class play_season:
 
-    def __init__(self, teams_file_path, schedule):
-        self.teams_file_path = teams_file_path
+    def __init__(self, teams, schedule):
+        self.teams           = teams
         self.schedule        = schedule
         self.game_record     = copy.deepcopy(self.schedule)
         self.standings       = self.generate_initial_standings_NHL()
@@ -11,11 +12,9 @@ class play_season:
     # Create a dictionary to hold wins, losses, OT losses for each team
     def generate_initial_standings_NHL(self):
         standings = {}
-        teams_file = open(self.teams_file_path, 'r')
-        for line in teams_file:
-            line_mod = line.replace('\n', '')
-            standings[line_mod.split(',')[0]] = {"wins": 0, "losses": 0, "OTlosses": 0, "points": 0, "ROW": 0,
-                                                 "div": line_mod.split(',')[1]}
+        for team in self.teams:
+            standings[team.name] = {"wins": 0, "losses": 0, "OTlosses": 0, "points": 0, "ROW": 0,
+                                                 "div": team.division }
         return standings
 
     # Run through the schedule and decide each game with a coin flip
@@ -95,12 +94,12 @@ class play_season:
         for i in xrange(div_cutoff):
             result[atlantic_sorted[i][0]] += 1
             result[metro_sorted[i][0]] += 1
-            east.pop(atlantic_sorted[i][0]);
+            east.pop(atlantic_sorted[i][0])
             east.pop(metro_sorted[i][0])
             result[central_sorted[i][0]] += 1
             result[pacific_sorted[i][0]] += 1
-            west.pop(central_sorted[i][0]);
-            west.pop(pacific_sorted[i][0]);
+            west.pop(central_sorted[i][0])
+            west.pop(pacific_sorted[i][0])
 
         east_sorted = self.chk_tiebreaks_NHL(self.sort_by_points_row(east))
         west_sorted = self.chk_tiebreaks_NHL(self.sort_by_points_row(west))
@@ -135,8 +134,10 @@ class play_season:
         checkedpairs = []
         for i in xrange(len(standings_sorted)):
             for j in xrange(len(standings_sorted)):
-                if i == j: continue
-                if [i, j] in checkedpairs or [j, i] in checkedpairs: continue
+                if i == j:
+                    continue
+                if [i, j] in checkedpairs or [j, i] in checkedpairs:
+                    continue
                 if standings_sorted[i][1]["points"] == standings_sorted[j][1]["points"] and standings_sorted[i][1][
                    "ROW"] == standings_sorted[j][1]["ROW"]:
                     if self.chk_head_to_head_NHL(standings_sorted[i][0], standings_sorted[j][0]) == standings_sorted[j][0]:
@@ -147,18 +148,21 @@ class play_season:
     # Check the head to head record for tie breaking
     # Rules (per NHL.com):
     # Team with that earned the most points in games between the two teams wins the tie-break
-    # When the teams played an odd number of games, points earned in the first game played in the city that had the extra game shall not be included.
-    # The last fallback if the teams are still tied is the team with the greater goal differential -- not clear what to do there, just return random for now
+    # When the teams played an odd number of games, points earned in the first game played in the city that had
+    # the extra game shall not be included.
+    # The last fallback if the teams are still tied is the team with the greater goal differential -- not clear what
+    # to do there, just return random for now
     def chk_head_to_head_NHL(self, team1, team2):
 
         # First for convenience find the head-to-head games in our self.game_record and put them in a smaller dictionary
         head_to_head = dict()
         for game in self.game_record:
-            if (self.game_record[game]["home"] == team1 or self.game_record[game]["visitor"] == team1) and (
-                    self.game_record[game]["home"] == team2 or self.game_record[game]["visitor"] == team2):
+            if (self.game_record[game]["home"] == team1 or self.game_record[game]["visitor"] == team1) and \
+               (self.game_record[game]["home"] == team2 or self.game_record[game]["visitor"] == team2):
                 head_to_head[game] = (self.game_record[game])
 
-        # For odd numbers of games, determine which team had more home games and "pop" their first home game from head_to_head
+        # For odd numbers of games, determine which team had more home games and
+        # "pop" their first home game from head_to_head
         # This could probably be improved
         if len(head_to_head) % 2 != 0:
             count = {team1: 0, team2: 0}
@@ -169,7 +173,8 @@ class play_season:
             else:
                 pop_team = team2
             for i in xrange(len(head_to_head_sorted)):
-                if count[head_to_head_sorted[i][1]["home"]] == pop_team: head_to_head.pop(head_to_head_sorted[i][0])
+                if count[head_to_head_sorted[i][1]["home"]] == pop_team:
+                    head_to_head.pop(head_to_head_sorted[i][0])
 
         # Determine how many points each team earned in their games against each other
         team1_pts = 0
@@ -177,10 +182,12 @@ class play_season:
         for game in head_to_head:
             if self.game_record[game]["winner"] == team1:
                 team1_pts += 2
-                if self.game_record[game]["OT"] == "OT" or self.game_record[game]["OT"] == "SO": team2_pts += 1
+                if self.game_record[game]["OT"] == "OT" or self.game_record[game]["OT"] == "SO":
+                    team2_pts += 1
             elif self.game_record[game]["winner"] == team2:
                 team2_pts += 2
-                if self.game_record[game]["OT"] == "OT" or self.game_record[game]["OT"] == "SO": team1_pts += 1
+                if self.game_record[game]["OT"] == "OT" or self.game_record[game]["OT"] == "SO":
+                    team1_pts += 1
 
         if team1_pts > team2_pts:
             return team1
@@ -192,7 +199,7 @@ class play_season:
     # Print sorted standings, for debugging
     # This doesn't get called anywhere at the moment
     def print_standings_sorted(self):
-        standings_sorted = self.chk_tiebreaks_NHL(sort_by_points_row(self.standings))
+        standings_sorted = self.chk_tiebreaks_NHL(self.sort_by_points_row(self.standings))
         print('{:<25}'.format('Team'),
               '{:<10}'.format('Wins'),
               '{:<10}'.format('Losses'),
