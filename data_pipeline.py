0f717890ec5ced4fc8503e27dc8892fc7454ce9b
jtaenzer <mars.taenzer@gmail.com>
2020-05-12 10:44:01 -0400

    Class for extracting and transforming data

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,1 +1,31 @@
-from database_maker import DatabaseMakerMySQL
+import pandas as pd
+import numpy as np
+
+
+class DataPipeline:
+
+    def __init__(self, db):
+        self.db = db
+
+    def extract_data(self, table_name, step=5, variables=[], output="game_outcome"):
+        var_str = ", ".join(variables) + ", " + output
+        cols = variables
+        if not variables:
+            var_str = "*"
+            self.db.cursor.execute("DESCRIBE {0}".format(table_name))
+            cols = [col[0] for col in self.db.cursor.fetchall()]
+
+        gamelog = pd.read_sql_query("SELECT {0} FROM {1} WHERE game_outcome != ''".format(var_str, table_name),
+                                    self.db.db)
+
+        data_list = []
+        for i in range(1, len(gamelog)):
+            averages = []
+            for col in cols:
+                if i < step:
+                    averages.append(np.sum(gamelog[col].iloc[0:i])/i)
+                else:
+                    averages.append(np.sum(gamelog[col].iloc[i-step:i])/step)
+            data_list.append(averages)
+            data_list[-1].append(gamelog[output].iloc[i])
+        return pd.DataFrame(np.array(data_list), columns=variables + [output])
