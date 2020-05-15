d5792df270f18b45acdb16b754cfc95b1b38b5dd
jtaenzer <mars.taenzer@gmail.com>
2020-05-15 12:08:01 -0400

    Added a bool controlling creation of average game data tables

--- dbconfig.py
+++ dbconfig.py
@@ -1,10 +1,11 @@
-remake_db = False
-remake_league_structure_tables = False
-remake_schedule_tables = False
-fill_gamelog_tables = False
-remake_gamelog_tables = False
-remake_sim_result_table = False
-remake_season_result_tables = False
+remake_db = True
+remake_league_structure_tables = True
+remake_schedule_tables = True
+fill_gamelog_tables = True
+remake_gamelog_tables = True
+remake_sim_result_table = True
+remake_season_result_tables = True
+remake_game_avg_tables = True
 
 years = ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
 
